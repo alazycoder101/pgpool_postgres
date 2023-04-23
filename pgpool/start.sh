08d9f1d84cb375023726dac2c5ec2b387237f615
alazycoder <alazycoder101@gmail.com>
2023-04-23 19:32:03 +0100

    always remove the pid file before starting up

---
+++ pgpool/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# clean up pidfile
+rm -rf /var/run/pgpool/pgpool.pid
+# Start Pgpool-II
+echo "Starting Pgpool-II..."
+${PGPOOL_INSTALL_DIR}/bin/pgpool -n \
+    -f ${PGPOOL_INSTALL_DIR}/etc/pgpool.conf \
+    -F ${PGPOOL_INSTALL_DIR}/etc/pcp.conf \
+    -a ${PGPOOL_INSTALL_DIR}/etc/pool_hba.conf \
+    -k ${PGPOOL_INSTALL_DIR}/etc/.pgpoolkey
